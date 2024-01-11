7f18e7ce32ea3cff1a85452b1e3cf0f2d76f9c68
Mathys Deshaies <mathys.deshaies@icloud.com>
2024-01-11 14:03:28 -0500

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import SplineApp from '@/components/spline';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export default function Home() {
-  return (
-    <div>
-      <SplineApp/>
-    </div>
-  )
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <SplineApp />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
